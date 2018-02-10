b4a63e2c7f6b63b85cb745c4288bbe40c29b9fb9
DhruvishaSupeda <dhruvishasupeda@gmail.com>
2018-02-10 23:29:13 +0000

    Initial commit of MakeCircles

---
+++ problemsheet1/MakeCircles.java
@@ -0,0 +1,20 @@
+package uk.ac.sheffield.com1003.problemsheet1;
+import sheffield.*;
+
+
+
+public class MakeCircles{
+	public static void main(String[] args) {
+		//Constants to store the canvas width and height
+		final int CANVAS_WIDTH = 360;
+		final int CANVAS_HEIGHT = 360;
+		
+		//Instantiating the two circle objects
+		Circle circle1 = new Circle(20,20,20);
+		Circle circle2 = new Circle(40,40,40);
+		
+		EasyGraphics g = new EasyGraphics(CANVAS_WIDTH,CANVAS_HEIGHT);
+	}
+	
+	
+}
