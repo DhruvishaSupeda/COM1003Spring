306c6bedecfbf0660d5097bd90b186ffa32c2d26
DhruvishaSupeda <dhruvishasupeda@gmail.com>
2018-02-10 23:27:09 +0000

    Initial commit

---
+++ problemsheet1/Circle.java
@@ -0,0 +1,53 @@
+/*
+ * Circle.java  	1.1 30/01/2018
+ * 
+ * This is a template for problem sheet 1
+ *
+ * Copyright (c) University of Sheffield 2018
+ */
+
+/** 
+* TryArrayList.java
+* 
+* 
+* @version 1.1 30/01/2018
+* 
+* @author Richard Clayton
+*/
+
+package uk.ac.sheffield.com1003.problemsheet1;
+
+public class Circle {
+	// constructor
+	public Circle( double r, int x, int y ){
+		radius = r;
+		xPosition = x;
+		yPosition = y;
+	}
+	
+	// methods -- you should remove the comments, and complete the method bodies
+	/*
+	public double getArea(){
+		// add your own code here to return the area of the circle
+	}
+	public double getCircumference(){
+		// add your own code here to return the circumference of the circle
+	}
+	public double getRadius(){
+		return radius;
+	}
+	public void setPosition( double newX, double newY ){
+		this.xPosition = newX;
+		this.yPosition = newY;
+	}	
+	public void setRadius( double newRadius ){
+		this.radius = newRadius;
+	}
+	*/	
+	
+	
+	// instance variables
+	private double radius;
+	private int xPosition;
+	private int yPosition;
+}
