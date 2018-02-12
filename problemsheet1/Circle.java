6b7c1e906e0bba241945fd6a57cd2d353be83322
Dhruvisha <dhruvishasupeda@gmail.com>
2018-02-12 20:51:21 +0000

    Javadoc comments added

--- problemsheet1/Circle.java
+++ problemsheet1/Circle.java
@@ -27,32 +27,52 @@ public class Circle {
 	
 	// methods -- you should remove the comments, and complete the method bodies
 	
+	//Constant used for the value of Pi when working out the area and circumference
 	final double PI = 3.142;
 	
+	/**
+	 * Calculates and returns the area of the circle it is called for
+	 * @return the area of the object
+	 */
 	public double getArea(){
-		//Works out the area of the circle
-		double area = (radius)*(radius)*PI;
-		//Returns the area to the MakeCircles class
+		double area = (Math.pow(radius, 2))*PI;
 		return area;
 	}
+	
+	/**
+	 * Calculates and returns the circumference of the circle it is called for 
+	 * @return the circumference of the circle
+	 */
 	public double getCircumference(){
-		//Works out the circumference of the circle
 		double circumference = 2*(radius)*PI;
-		//Returns the circumference to the MakeCircles class
 		return circumference;
 	}
+	
+	/**
+	 * Returns the radius of the circle
+	 * @return the radius of the circle
+	 */
 	public double getRadius(){
 		return radius;
 	}
+	
+	/**
+	 * Sets the x and y coordinates of the circle
+	 * @param newX	The new x value to be given to the circle
+	 * @param newY	The new y value to be given to the circle
+	 */
 	public void setPosition( double newX, double newY ){
 		xPosition = (int)newX;
 		yPosition = (int)newY;
 	}	
+	
+	/**
+	 * Sets a new radius for the circle
+	 * @param newRadius	The new radius to be given to the circle
+	 */
 	public void setRadius( double newRadius ){
 		radius = newRadius;
 	}
-	
-	
 	
 	// instance variables
 	private double radius;
