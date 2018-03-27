49cd8427e798d66823e3b7816f04efffb3c195b3
Dhruvisha <dhruvishasupeda@gmail.com>
2018-03-27 16:17:59 +0100

    Attempted Rook adding legal moves

--- assignment2018/Board.java
+++ assignment2018/Board.java
@@ -15,8 +15,15 @@ public class Board {
     Piece playingBoard[][] = new Piece[8][8];
     Pieces initPiecesW = new Pieces(playingBoard, PieceCode.WHITE);
     Pieces initPiecesB = new Pieces(playingBoard, PieceCode.BLACK);
-
-
+    for (int i = 0; i < playingBoard.length; i++){
+      for (int j = 0; j < playingBoard.length; j++){
+        System.out.print(playingBoard[i][j].getChar); //MAYBE????
+    }
+  } //MAY HAVE TO PUT THE INITIAL BOARD IN CHESS.JAVA RATHER THAN HERE
   }
+
+  public static void main(String[] args) {
+    initBoard();
   }
+
 }
--- assignment2018/Rook.java
+++ assignment2018/Rook.java
@@ -18,3 +18,177 @@ public class Rook extends Piece {
         else
             return blackPawn();
     }
+
+    public ArrayList<Move> moveYCoords() {
+      //???? MAYBE NEED A WAY TO PUT THEM IN THE SAME ARRAYLIST IDK
+      //IF ITS BY REFERENCE, CAN DO IT???
+      //BUT ALSO NEED TO RETURN IT IDK DON'T WANT TO return
+    }
+
+    // method to return list of legal moves for a white rook
+    private ArrayList<Move> whiteRook() {
+        // obtain current co-ordinates
+        int x = this.getX();
+        int y = this.getY();
+        int i = 1;
+
+        // Create a new vector to store legal whiteMoves
+        ArrayList<Move> whiteMoves = new ArrayList<Move>();
+
+        // set up m to refer to a Move object
+        Move theMove = null;
+
+        //GOING UP AS ROOK - while not occupied, or occupied by black AND in range
+        while (()!getBoard().occupied(x, y+1) || getBoard().occupied(x, y+1)
+                && (getBoard().getPiece(x, y+1).getColour() != this.getColour()))
+                && (!getBoard().outOfRange(x, y+1)) ) {
+
+            if (getBoard().occupied(x, y+1) && (getBoard().getPiece(x, y+1).getColour() != this.getColour())) {
+              theMove = new Move(this, x, y, x, y+1, true);
+              whiteMoves.add(theMove);
+            }
+            else {
+              theMove = new Move(this, x, y, x, y+1, false);
+              whiteMoves.add(theMove);
+            }
+            i+=1;
+        }
+
+        i=-1;
+        while (()!getBoard().occupied(x, y+1) || getBoard().occupied(x, y+1)
+                && (getBoard().getPiece(x, y+1).getColour() != this.getColour()))
+                && (!getBoard().outOfRange(x, y+1)) ) {
+
+            if (getBoard().occupied(x, y+1) && (getBoard().getPiece(x, y+1).getColour() != this.getColour())) {
+              theMove = new Move(this, x, y, x, y+1, true);
+              whiteMoves.add(theMove);
+            }
+            else {
+              theMove = new Move(this, x, y, x, y+1, false);
+              whiteMoves.add(theMove);
+            }
+            i+=1;
+        }
+
+
+        i=1;
+        while (()!getBoard().occupied(x+i, y) || getBoard().occupied(x+i, y)
+                && (getBoard().getPiece(x+i, y).getColour() != this.getColour()))
+                && (!getBoard().outOfRange(x+i, y)) ) {
+
+            if (getBoard().occupied(x+i, y) && (getBoard().getPiece(x+i, y).getColour() != this.getColour())) {
+              theMove = new Move(this, x, y, x+i, y, true);
+              whiteMoves.add(theMove);
+            }
+            else {
+              theMove = new Move(this, x, y, x+i, y, false);
+              whiteMoves.add(theMove);
+            }
+            i+=1;
+        }
+
+        i=-1;
+        while (()!getBoard().occupied(x+i, y) || getBoard().occupied(x+i, y)
+                && (getBoard().getPiece(x+i, y).getColour() != this.getColour()))
+                && (!getBoard().outOfRange(x+i, y)) ) {
+
+            if (getBoard().occupied(x+i, y) && (getBoard().getPiece(x+i, y).getColour() != this.getColour())) {
+              theMove = new Move(this, x, y, x+i, y, true);
+              whiteMoves.add(theMove);
+            }
+            else {
+              theMove = new Move(this, x, y, x+i, y, false);
+              whiteMoves.add(theMove);
+            }
+            i+=1;
+        }
+
+        if (whiteMoves.isEmpty())
+            return null;
+        return whiteMoves;
+    }
+
+    // method to return list of legal moves for a black rook
+    private ArrayList<Move> blackRook() {
+        // obtain current co-ordinates
+        int x = this.getX();
+        int y = this.getY();
+        int i = 1;
+
+        // Create a new vector to store legal blackMoves
+        ArrayList<Move> blackMoves = new ArrayList<Move>();
+
+        // set up m to refer to a Move object
+        Move theMove = null;
+
+        //GOING UP AS ROOK - while not occupied, or occupied by black AND in range
+        while (()!getBoard().occupied(x, y+1) || getBoard().occupied(x, y+1)
+                && (getBoard().getPiece(x, y+1).getColour() != this.getColour()))
+                && (!getBoard().outOfRange(x, y+1)) ) {
+
+            if (getBoard().occupied(x, y+1) && (getBoard().getPiece(x, y+1).getColour() != this.getColour())) {
+              theMove = new Move(this, x, y, x, y+1, true);
+              blackMoves.add(theMove);
+            }
+            else {
+              theMove = new Move(this, x, y, x, y+1, false);
+              blackMoves.add(theMove);
+            }
+            i+=1;
+        }
+
+        i=-1;
+        while (()!getBoard().occupied(x, y+1) || getBoard().occupied(x, y+1)
+                && (getBoard().getPiece(x, y+1).getColour() != this.getColour()))
+                && (!getBoard().outOfRange(x, y+1)) ) {
+
+            if (getBoard().occupied(x, y+1) && (getBoard().getPiece(x, y+1).getColour() != this.getColour())) {
+              theMove = new Move(this, x, y, x, y+1, true);
+              blackMoves.add(theMove);
+            }
+            else {
+              theMove = new Move(this, x, y, x, y+1, false);
+              blackMoves.add(theMove);
+            }
+            i+=1;
+        }
+
+
+        i=1;
+        while (()!getBoard().occupied(x+i, y) || getBoard().occupied(x+i, y)
+                && (getBoard().getPiece(x+i, y).getColour() != this.getColour()))
+                && (!getBoard().outOfRange(x+i, y)) ) {
+
+            if (getBoard().occupied(x+i, y) && (getBoard().getPiece(x+i, y).getColour() != this.getColour())) {
+              theMove = new Move(this, x, y, x+i, y, true);
+              blackMoves.add(theMove);
+            }
+            else {
+              theMove = new Move(this, x, y, x+i, y, false);
+              blackMoves.add(theMove);
+            }
+            i+=1;
+        }
+
+        i=-1;
+        while (()!getBoard().occupied(x+i, y) || getBoard().occupied(x+i, y)
+                && (getBoard().getPiece(x+i, y).getColour() != this.getColour()))
+                && (!getBoard().outOfRange(x+i, y)) ) {
+
+            if (getBoard().occupied(x+i, y) && (getBoard().getPiece(x+i, y).getColour() != this.getColour())) {
+              theMove = new Move(this, x, y, x+i, y, true);
+              blackMoves.add(theMove);
+            }
+            else {
+              theMove = new Move(this, x, y, x+i, y, false);
+              blackMoves.add(theMove);
+            }
+            i+=1;
+        }
+
+        if (blackMoves.isEmpty())
+            return null;
+        return blackMoves;
+    }
+
+}
