9d21c8311b8e4a62af517687369a8d22e57b3417
Dhruvisha <dhruvishasupeda@gmail.com>
2018-05-02 02:03:11 +0100

    Buttons exist

--- assignment2018/GraphicalDisplay.java
+++ assignment2018/GraphicalDisplay.java
@@ -6,23 +6,25 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GraphicalDisplay extends JFrame implements Display {
+
+    private JButton[][] boardButtons = new JButton[8][8];
+
     public GraphicalDisplay() {
         setTitle("Chess");
 
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension screenDimensions = toolkit.getScreenSize();
-        setSize(screenDimensions.width/2, screenDimensions.height/2);
+        setSize(screenDimensions.width / 2, screenDimensions.height / 2);
 
-        setSize(900,600);
+        setSize(900, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setVisible(true);
 
         Container contentPane = getContentPane();
-        contentPane.setLayout(new GridLayout(8,8));
+        contentPane.setLayout(new GridLayout(8, 8));
 
-        JButton[][] boardButtons = new JButton[8][8];
-        for (int y = 0; y<8; y++) {
-            for (int x=0; x<8; x++) {
+        for (int y = 0; y < 8; y++) {
+            for (int x = 0; x < 8; x++) {
                 boardButtons[x][y] = new JButton();
                 contentPane.add(boardButtons[x][y]);
             }
@@ -33,8 +35,42 @@ public class GraphicalDisplay extends JFrame implements Display {
         Board playingBoard = myPieces.getPiece(1).getBoard();
         System.out.println(playingBoard.toString());
 
-        case (myPieces.getPiece(i).getChar()): {
 
+        for (int y = 0; y<8; y++) {
+            for (int x = 0; x<8; x++) {
+                if (playingBoard.getPiece(x,y) != null) {
+                    System.out.println(playingBoard.getPiece(x,y));
+                    switch (playingBoard.getPiece(x, y).getChar()) {
+                        case PieceCode.PAWNWHITE:
+                            break;
+                        case PieceCode.KNIGHTWHITE:
+                            break;
+                        case PieceCode.BISHOPWHITE:
+                            break;
+                        case PieceCode.ROOKWHITE:
+                            break;
+                        case PieceCode.QUEENWHITE:
+                            break;
+                        case PieceCode.KINGWHITE:
+                            break;
+                        case PieceCode.PAWNBLACK:
+                            break;
+                        case PieceCode.KNIGHTBLACK:
+                            break;
+                        case PieceCode.BISHOPBLACK:
+                            break;
+                        case PieceCode.ROOKBLACK:
+                            break;
+                        case PieceCode.QUEENBLACK:
+                            break;
+                        case PieceCode.KINGBLACK:
+                            break;
+                        default:
+                            break;
+
+                    }
+                }
+            }
         }
 
     }
