414a37be6886daae6544c4471b3e1a89339209d8
Dhruvisha <dhruvishasupeda@gmail.com>
2018-05-07 12:04:59 +0100

    Moving laptops, putting input in

--- assignment2018/GraphicalDisplay.java
+++ assignment2018/GraphicalDisplay.java
@@ -7,18 +7,20 @@ import java.awt.*;
 
 public class GraphicalDisplay extends JFrame implements Display {
 
-    private JButton[][] boardButtons = new JButton[8][8];
+    private JLabel[][] boardButtons = new JLabel[8][8];
+    private JLabel[] inputArray = new JLabel[8];
     private Container contentPane = getContentPane();
     private JPanel letterPanel = new JPanel();
-    JPanel numberPanel = new JPanel();
-    JPanel boardPanel = new JPanel();
+    private JPanel numberPanel = new JPanel();
+    private JPanel boardPanel = new JPanel();
+    private JPanel inputPanel = new JPanel();
 
     public GraphicalDisplay() {
         setTitle("Chess");
 
         Toolkit toolkit = Toolkit.getDefaultToolkit();
         Dimension screenDimensions = toolkit.getScreenSize();
-        setSize(screenDimensions.width / 2, screenDimensions.height / 2);
+        setSize(screenDimensions.width/2, screenDimensions.height);
 
         //setSize(1000, 600);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -29,22 +31,46 @@ public class GraphicalDisplay extends JFrame implements Display {
         letterPanel.setLayout(new GridLayout(1,0));
         numberPanel.setLayout(new GridLayout(0,1));
         boardPanel.setLayout(new GridLayout(0,8));
+        inputPanel.setLayout(new GridLayout(0,2));
 
 
         for (int y = 0; y < 8; y++) {
+            numberPanel.add(new JLabel(Integer.toString(y+1, JLabel.CENTER)));
+            letterPanel.add(new JLabel("A"));
             for (int x = 0; x < 8; x++) {
-                boardButtons[x][y] = new JButton();
+                boardButtons[x][y] = new JLabel();
+                boardButtons[x][y].setBorder(BorderFactory.createLineBorder(Color.black));
+                boardButtons[x][y].setHorizontalAlignment(JLabel.CENTER);
                 boardPanel.add(boardButtons[x][y]);
             }
         }
 
-        for (int i = 0; i<8; i++) {
-            numberPanel.add(new JButton(Integer.toString(i)));
-            letterPanel.add(new JButton("A"));
+        for (int i = 0; i<8; i++)
+            inputArray[i] = new JLabel();
+
+        inputArray[0].setText("Input:");
+        inputArray[1].setText(null);
+        JComboBox letterOne = new JComboBox();
+        JComboBox letterTwo = new JComboBox();
+        JComboBox numberOne = new JComboBox();
+        JComboBox numberTwo = new JComboBox();
+        for (int i = 1; i<9; i++) {
+            letterOne.addItem(('A' + i));
+            letterOne.addItem(('A' + i));
+            numberOne.addItem(i);
+            numberTwo.addItem(i);
         }
+
+        //inputPanel.setSize(screenDimensions.width/6, screenDimensions.height);
+        inputArray[2] = letterOne; //add to JLAbel array somehow idk
+        inputArray[3] = numberOne;
+        inputArray[4] = letterOne;
+        inputArray[5] = letterOne;
+
         contentPane.add(letterPanel, BorderLayout.NORTH);
         contentPane.add(numberPanel, BorderLayout.WEST);
         contentPane.add(boardPanel, BorderLayout.CENTER);
+        contentPane.add(inputPanel, BorderLayout.EAST);
         setVisible(true);
     }
 
--- assignment2018/TextDisplay.java
+++ assignment2018/TextDisplay.java
@@ -5,11 +5,6 @@ import assignment2018.codeprovided.PieceCode;
 public class TextDisplay implements Display {
 
   private char[][] pieceArray = new char[8][8];
-  //Counter so that every time both piece colours are put into object, displays board
-  private int bwCounter;
-  //Flags for white pieces and black pieces used
-  private boolean wFlag;
-  private boolean bFlag;
 
   public TextDisplay() {
     for (int i = 0; i < 8; i++) {
@@ -17,42 +12,9 @@ public class TextDisplay implements Display {
           pieceArray[i][j] = '.';
       }
     }
-    wFlag = false;
-    bFlag = false;
-  }
 
-  public void resetFlags() {
-      wFlag = false;
-      bFlag = false;
-  }
 
   public void displayBoard(Pieces myPieces) {
-    /*Piece newPiece;
-    char pieceChar;
-    int pieceColour;
-    for (int i = 0; i < myPieces.getNumPieces(); i++) {
-      //for (int j = 1; j < 9; j++) {
-        newPiece = myPieces.getPiece(i);
-        pieceChar = newPiece.getChar();
-        pieceColour = newPiece.getColour();
-        pieceArray[newPiece.getX()][newPiece.getY()] = newPiece.getChar();
-        if (newPiece.getColour() == PieceCode.BLACK)
-            bFlag = true;
-        else
-            wFlag = true;
-    }
-
-    if ((bFlag==true) && (wFlag==true)) {
-      System.out.println(" |A B C D E F G H");
-      System.out.println("- - - - - - - - -");
-      for (int i = 0; i < 8; i++) { //MAKE BOARD_WIDTH AND HEIGHT CONSTANTS
-          //8-i because it goes 8 downto 1 but can't change loop
-          System.out.print(8-i + "|");
-          for (int j = 0; j < 8; j++) {
-              System.out.print(pieceArray[j][i] + " ");
-          }
-          System.out.println();
-      }*/
       Board playingBoard = myPieces.getPiece(1).getBoard();
       System.out.println(" |A B C D E F G H");
       System.out.println("- - - - - - - - -");
